a3bf1ea295ede1cf086a08262b61987ad8251d64
jacques <jacques.troussard@gmail.com>
2021-12-03 02:01:27 -0500

    trying to get coveralls to work

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,28 @@
+
+const { defaults } = require('jest-config');
+
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  transform: {
+    // "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": "babel-jest",
+    // "^.+\\.mjs$": "babel-jest",
+  },
+  testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/", "<rootDir>/dist/", "<rootDir>/src/router/"],
+  coveragePathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/", "<rootDir>/dist/", "<rootDir>/src/router/"],
+  preset: '@vue/cli-plugin-unit-jest',
+  verbose: true,
+  moduleFileExtensions: ["js", "vue"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{js,jsx,vue}",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+    "!**/plugins/**",
+    "!**/coverage/**",
+    "!**/*.config.js",
+    "!**/main.js",
+    "!**/store/index.js",
+    "!**/nf_*",
+  ],
 }
+
